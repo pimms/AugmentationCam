1acea09323d18bf5fd660484aa1fc27350d488df
pimms <joakim.stien@gmail.com>
2013-09-26 16:40:25 +0200

    Added textured heightmap

--- src/pimms/oblig2/gfx/Object3D.java
+++ src/pimms/oblig2/gfx/Object3D.java
@@ -84,6 +84,7 @@ public abstract class Object3D {
 			gl.glDisable(GL10.GL_TEXTURE_2D);
 		}
 		
+		
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
 		gl.glDrawArrays(GL10.GL_TRIANGLES, 0, mVertCount);
 		
