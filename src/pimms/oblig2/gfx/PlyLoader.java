1618b329218799db2835f339cbeeff454ac1b4dd
pimms <joakim.stien@gmail.com>
2013-09-27 16:11:39 +0200

    Added an Activity before the AugmentationActivity.

---
+++ src/pimms/oblig2/gfx/PlyLoader.java
@@ -0,0 +1,24 @@
+package pimms.oblig2.gfx;
+
+/*
+ * Class to load Polygon File Format files.
+ * TODO: This shit
+ */
+public class PlyLoader {
+	
+	public boolean parseFile(String file) {
+		return false;
+	}
+	
+	public float[] getVertices() {
+		return null;
+	}
+	
+	public int[] getIndices() {
+		return null;
+	}
+	
+	public float[] getTexCoords() {
+		return null;
+	}
+}
